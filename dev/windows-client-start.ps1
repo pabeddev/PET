e2e51c50d25ed967eaeb623c505dfb0fb1c0be47
Pablo Julian Garay de Leon <143024751+MainLake@users.noreply.github.com>
2024-07-12 22:25:50 -0600

    Feat: add script for windows for run all proyect

--- dev/windows-client-start.ps1
+++ dev/windows-client-start.ps1
@@ -2,9 +2,25 @@ Write-Host "Cliente de desarrollo PET"
 Write-Host "Iniciando cliente de desarrollo..."
 if (-not (Test-Path -Path "../client/node_modules")) {
   Write-Host "Instalando dependencias..."
-  npm install
+  npm install --prefix ../client
 }else{
-  Write-Host "Dependencias ya instaladas"
+    Write-Host "Dependencias ya instaladas"
 }
-cd ../client
-npm run start
+
+# Existe el archivo .env?
+if (-not (Test-Path -Path "../client/.env")) {
+  Write-Host "Creando archivo .env..."
+  New-Item -Path "../client" -Name ".env" -ItemType "file"
+  # agregar las variables de entorno al archivo .env
+  Add-Content -Path "../client/.env" -Value 'REACT_APP_URL_API_DEV="http://localhost:3030/"'
+  Add-Content -Path "../client/.env" -Value 'REACT_APP_URL_API_PRODUCTION=""'
+}else{
+    Write-Host "Archivo .env ya existe"
+    # quitar todo el contenido del archivo .env
+    Clear-Content -Path "../client/.env"
+    # agregar las variables de entorno al archivo .env
+    Add-Content -Path "../client/.env" -Value 'REACT_APP_URL_API_DEV="http://localhost:3030/"'
+    Add-Content -Path "../client/.env" -Value 'REACT_APP_URL_API_PRODUCTION=""'
+}
+
+npm run start --prefix ../client
---
+++ dev/windows-run-all.ps1
@@ -0,0 +1,3 @@
+Write-Host "Proyecto de desarrollo PET"
+start powershell {..\dev\windows-client-start.ps1}
+start powershell {..\dev\windows-server-start.ps1}
