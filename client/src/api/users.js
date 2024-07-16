c361ad95ea68d67311a733c172c5c469d4911c85
Pablo Julian Garay de Leon <leonmaing@gmail.com>
2024-07-15 22:08:00 -0600

    Fix: chage logic for login component

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,25 +1,18 @@
 import { axiosInstance } from "../utilities/axiosInstance";
 
+// Las petciones a la API son generalmente reutilizadas por lo que manejar los errores dentro de la misma funcion no suele ser una idea muy buena
+// debido a que se pierde la capacidad de manejar los errores de manera personalizada en cada componente que haga uso de la funcion.
+// Por lo tanto, es mejor manejar los errores en el componente que haga uso de la funcion, de esta manera se puede personalizar el mensaje de error
+// Y por ello se retorna solamente la accion de la peticion a la API, en este caso un objeto de tipo Promise.
 export const createAccount = async (data) => {
     return axiosInstance.post("/users/", data);
 };
 
 export const loginUser = async (data) => {
-  try {
-    const response = await axiosInstance.post("/auth/login/", data);
-    console.log(response);
-    return response;
-  } catch (error) {
-    console.log(error);
-    if (
-      error.response.status === 400 ||
-      error.response.status === 401 ||
-      error.response.status === 404
-    ) {
-      return { error: "El correo electronico o la contrasena no son validos" };
-    }
-  }
+    return axiosInstance.post("/auth/login/", data);
 };
+
+
 
 export const getPetsUser = async (token) => {
   const config = {
