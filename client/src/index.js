74eb4edba0a96b5d1c9dd9707c2461cec4e2fd89
Pablo Julian Garay de Leon <leonmaing@gmail.com>
2024-07-23 09:35:13 -0600

    Fix: chage logic for components

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,12 @@ import App from "./App";
 import Initializer from "./routes/initializer/Initializer";
 import { BrowserRouter } from "react-router-dom";
 
+// Fuentes de Google
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 ReactDOM.render(
   <Initializer>
     <BrowserRouter>
